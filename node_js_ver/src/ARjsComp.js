86f1aceda8dc837f43d59d056c03b0af96290fa8
emor1 <57478311+emor1@users.noreply.github.com>
2022-12-12 20:28:51 +0900

    とりあえず基本が完成

---
+++ node_js_ver/src/ARjsComp.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {Entity, Scene} from 'aframe-react';
+import 'babel-polyfill';
+import 'aframe'
+import 'arjs'
+// import { ARjs } from '@ar-js-org/ar.js';
+
+export default class ARjsComp extends React.Component {
+
+  componentDidMount(){
+  }
+
+  render (){
+    return(
+      <div
+          style={{ width: "800px", height: "800px" }}
+          ref={mount => { this.mount = mount}}
+      >
+      <Scene>
+        <a-scene embedded arjs>
+          <a-marker preset="hiro">
+            <a-cone position='0 0.5 0' rotation="0 0 0" radius-bottom="0.5" material='opacity: 0.75; color: red;'></a-cone>
+          </a-marker>
+          <Entity primitive="a-camera"></Entity>
+        </a-scene>
+       </Scene>
+      </div>
+    )
+  }
+
+}
---
+++ node_js_ver/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ARjsComp from "./ARjsComp";
+
+function App(){
+    return(
+        <div>
+            <ARjsComp />
+        </div>
+    )
+}
+
+export default App;
